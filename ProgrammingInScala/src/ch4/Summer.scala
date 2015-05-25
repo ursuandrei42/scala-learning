80028419141ef15962d9a0833d95d87ff4ded638
ursuandrei42 <andrei_master0@yahoo.com>
2015-05-25 20:56:00 +0300

    -format

--- ProgrammingInScala/src/ch4/Summer.scala
+++ ProgrammingInScala/src/ch4/Summer.scala
@@ -3,8 +3,10 @@ package ch4
 import ChecksumAccumulator.calculate
 
 object Summer {
+  
   def main(args: Array[String]) {
     for (arg <- args)
       println(arg + ": " + calculate(arg))
   }
+  
 }
